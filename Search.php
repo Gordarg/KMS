ac5bce9d45678c875145d28ced0c7b84fa77b550
tayyebi <tayyebimohammadreza@gmail.com>
2018-02-23 21:24:53 +0330

    More info for search

--- Search.php
+++ Search.php
@@ -3,7 +3,12 @@ include ('core/public-header.php');
 require_once 'semi-orm/Posts.php';
 use orm\Posts;
 ?>
-<!-- SEARCH PAGE HERE -->
+<form method="GET" action="Search.php">
+    <input type="text" name="Q" placeholder="عبارت را وارد نمائید" value="<?= $_GET['Q'] ?>" />
+    <input type="submit" value = "جستجو" />
+</form>
+
 <?php
+echo $_GET['Q']; // Search by this
 include ('core/public-footer.php');
 ?>
