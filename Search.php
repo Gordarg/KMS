44ad48bcf326068613e3ec7b2beecc6b9966db59
tayyebi <tayyebimohammadreza@gmail.com>
2018-02-23 21:45:29 +0330

    Null value bug

--- Search.php
+++ Search.php
@@ -1,14 +1,17 @@
 <?php
 include ('core/public-header.php');
+require_once 'core/functionalities.php';
+use core\functionalities;
 require_once 'semi-orm/Posts.php';
 use orm\Posts;
+$Q = (new functionalities())->ifexistsidx($_GET,'Q');
 ?>
 <form method="GET" action="Search.php">
-    <input type="text" name="Q" placeholder="عبارت را وارد نمائید" value="<?= $_GET['Q'] ?>" />
+    <input type="text" name="Q" placeholder="عبارت را وارد نمائید" value="<?= $Q ?>" />
     <input type="submit" value = "جستجو" />
 </form>
 
 <?php
-echo $_GET['Q']; // Search by this
+echo $Q; // Search by this
 include ('core/public-footer.php');
 ?>
