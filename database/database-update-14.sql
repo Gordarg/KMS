83d28c2181b88ad2dd1f340125727ef993178233
maysam13765717 <maysamnegahdar5717@gmail.com>
2018-03-03 20:58:33 +0330

    View's SELECT contains a subquery in the FROM clause

--- database/database-update-14.sql
+++ database/database-update-14.sql
@@ -1,3 +1,28 @@
+CREATE OR REPLACE
+View `post_contents` AS
+SELECT
+        `posts`.*,
+        `categories`.`Name` AS `CategoryName`,
+        `users`.`username`,
+        (
+            SELECT  Submit as D1 FROM posts WHERE `Content` IS NOT NULL and `MasterId` = `posts`.`MasterId` order by `Submit` desc limit 1
+		) as D1
+		,
+		(
+            SELECT  Submit as D2 FROM posts WHERE `ContentDeleted` = 1 and `MasterId` = `posts`.`MasterId` order by `Submit` desc limit 1
+		) as D2
+    FROM
+        ((`posts`
+        JOIN `users` ON ((`posts`.`UserId` = `users`.`id`)))
+        JOIN `categories` ON ((`posts`.`CategoryId` = `categories`.`Id`)))
+    WHERE
+        (`posts`.`Id` IN (SELECT 
+                MAX(`posts`.`Id`)
+            FROM
+                `posts`
+            GROUP BY `posts`.`MasterId`)
+            AND (`posts`.`Deleted` = 0));
+
 CREATE 
      OR REPLACE
 VIEW `post_details` AS
@@ -17,24 +42,4 @@ VIEW `post_details` AS
 		T.`Index`,
 		T.`Status`,
 		CASE WHEN D2 >= D1 THEN NULL ELSE `Content` END AS `Content`
-        FROM
-        (SELECT
-        `posts`.*,
-        `categories`.`Name` AS `CategoryName`,
-        `users`.`username`,
-        (SELECT  Submit as D1 FROM posts WHERE `Content` IS NOT NULL and `MasterId` = `posts`.`MasterId` order by `Submit` desc limit 1
-		) as D1
-		,
-		(SELECT  Submit as D2 FROM posts WHERE `ContentDeleted` = 1 and `MasterId` = `posts`.`MasterId` order by `Submit` desc limit 1
-		) as D2
-    FROM
-        ((`posts`
-        JOIN `users` ON ((`posts`.`UserId` = `users`.`id`)))
-        JOIN `categories` ON ((`posts`.`CategoryId` = `categories`.`Id`)))
-    WHERE
-        (`posts`.`Id` IN (SELECT 
-                MAX(`posts`.`Id`)
-            FROM
-                `posts`
-            GROUP BY `posts`.`MasterId`)
-            AND (`posts`.`Deleted` = 0))) T;
+        FROM `post_contents` T;
