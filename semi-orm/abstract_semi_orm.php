97d8318732c9938aa6e56a7fbad58e995912f9dd
tayyebi <tayyebimohammadreza@gmail.com>
2018-04-26 10:56:05 +0430

    ignore take and limit

--- semi-orm/abstract_semi_orm.php
+++ semi-orm/abstract_semi_orm.php
@@ -23,7 +23,12 @@ class abstract_semi_orm implements semi_orm
     function ToList($Skip = 0 , $Take = 10, $OrderField = 'Id', $OrderArrange = 'ASC', $Clause = '')
     {
         /* TODO: Take and Skip -1 will disable those parameters */
-        $query = "SELECT * FROM `" . $this->table . "` " . $Clause . " ORDER BY `" . $OrderField . "` " . $OrderArrange . " LIMIT ". $Take . " OFFSET " . $Skip . ";";
+        $query = "SELECT * FROM `" . $this->table . "` " . $Clause . " ORDER BY `" . $OrderField . "` " . $OrderArrange;
+        if ($Take != -1)
+            $query .= " LIMIT ". $Take;
+        if ($Skip != -1)
+            $query .= " OFFSET " . $Skip;
+        $query .= ";";
         $result = mysqli_query($this->conn, $query);
         $rows = array();
         if ($result)
