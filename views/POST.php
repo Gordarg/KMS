b024a1f2ad6f1f12bd90bb71da33f32eddd8409a
tayyebi <tayyebimohammadreza@gmail.com>
2018-06-16 10:26:14 +0430

    Bug!

--- views/POST.php
+++ views/POST.php
@@ -13,14 +13,14 @@ switch ($_GET["level"])
         echo '<li><a href="view.php?lang="' . $row['Language'] . '"&id=' . $row['MasterID'] . '">';
         echo '  <img src="download.php?id=' . $row['MasterID'] . '">';
         echo '  <h1>' . $row['Title'] . '</h1><br>';
-        echo '  <span>' . $Parsedown->text($functionalitiesInstance->makeAbstract($row['Body'], 120)) . '</span>';
+        echo '  <span>' . $functionalitiesInstance->makeAbstract($Parsedown->text($row['Body']), 120) . '</span>';
         echo '</a></li>';  
         break;
     case "1":
         echo '<article>';
         echo '<img src="download.php?id=' . $row['MasterID'] . '" alt="' . $row["Title"] . '" >';
         echo '<h3><a href="view.php?lang=' . $row['Language'] . '&id=' . $row['MasterID'] . '">' . $row['Title'] . '</h3></a>';
-        echo '<p>' . $Parsedown->text($functionalitiesInstance->makeAbstract($row['Body'], 480))  . '</p>';
+        echo '<p>' . $functionalitiesInstance->makeAbstract($Parsedown->text($row['Body']), 480)  . '</p>';
         echo '</article>';
         break;
     case "2":
@@ -29,7 +29,7 @@ switch ($_GET["level"])
         echo '<div>';
         echo '<h4><b>' . $row['Username'] . '</b></h4>';
         echo '<h2><a href="view.php?lang=' . $row['Language'] . '&id=' . $row['MasterID'] . '">' . $row['Title'] . '</a></h2>';
-        echo $Parsedown->text($functionalitiesInstance->makeAbstract($row['Body'], 960, "<img>")) ;
+        echo $functionalitiesInstance->makeAbstract($Parsedown->text($row['Body']), 960, "<img>");
         echo '</div>';
         echo '</article>';
         break;
