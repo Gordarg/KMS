0ebfe7b15f082a331e9753d94608748cdff4f6bf
tayyebi <tayyebimohammadreza@gmail.com>
2018-07-27 22:23:26 +0430

    first stage of form builder

--- forms/QUST.php
+++ forms/QUST.php
@@ -4,53 +4,12 @@
 <input type="hidden" name="index" value="<?= $Index ?>" />
 <input type="hidden" name="refrenceid" value="<?= $RefrenceID ?>" />
 <input type="hidden" name="status" value="<?= $Status ?>" />
+<input type="hidden" name="title" value="<?= $Title ?>" />
 <input type="hidden" name="level" value="<?= $Level ?>" />
 <input type="hidden" name="language" value="<?= $_COOKIE["LANG"] ?>" />
-
-<label for="title"><?= $functionalitiesInstance->label("عنوان"); ?></label>
-<input name="title" placeholder="<?= $functionalitiesInstance->label("عنوان"); ?>" type="text" value="<?= $Title ?>" />
-<label for="body"><?= $functionalitiesInstance->label("متن"); ?></label>
-<textarea name="body"><?= $Body  ?></textarea>
-
-<!------------------------------>
-<div id="sjfb-body">
-<div id="sjfb-wrap">
-
-<h1>Simple Jquery Form Builder - Demo</h1>
-
-<p>The form builder below was loaded in from a json string.<br>
-    <a href="form.html">View form (rendered from the demo json string) here</a>.
-</p>
-
 <div class="alert hide">
-    <h2>Success! Form saved.</h2>
-    <p>Here is what your saved form will look like in your database:</p>
-    <textarea></textarea>
+    <textarea name="body"><?= $Body ?></textarea>
 </div>
-
-<div class="add-wrap">
-    <h3>Add Field:</h3>
-    <ul id="add-field">
-        <li><a id="add-text" data-type="text" href="#">Single Line Text</a></li>
-        <li><a id="add-textarea" data-type="textarea" href="#">Multi Line Text</a></li>
-        <li><a id="add-select" data-type="select" href="#">Select Box (Drop down list)</a></li>
-        <li><a id="add-radio" data-type="radio" href="#">Radio Buttons</a></li>
-        <li><a id="add-checkbox" data-type="checkbox" href="#">Checkboxes</a></li>
-        <li><a id="add-agree" data-type="agree" href="#">Agree Box</a></li>
-    </ul>
-</div>
-
-<form id="sjfb" novalidate>
-    <div id="form-fields">
-    </div>
-    <button type="submit" class="submit">Save Form</button>
-</form>
-
-</div>
-</div>
-
-<!------------------------------>
-
 <?php
 if ($Id == null ) {
     echo '<input type="submit" name="insert" value="' . $functionalitiesInstance->label("ارسال") . '" />';
@@ -60,3 +19,4 @@ if ($Id == null ) {
 }
     echo '<a href="index.php">' . $functionalitiesInstance->label("انصراف") . '</a>';
 ?>
+
--- question.php
+++ question.php
@@ -2,32 +2,15 @@
 include_once ('core/init.php');
 include ('core/secure.php');
 include ('forms/submit.php');
-// include ('master/public-header.php');
+include ('master/public-header.php');
 ?>
-<link href="css/question-php.css" rel="stylesheet" type="text/css" />
 
-<!-- jQuery JS -->
-<script src="js/master.js"></script> <!-- jQuery v1 should also work fine -->
-<script src="js/jquery-ui.js" type="text/javascript" ></script> <!-- for sortable -->
-
-<!-- SJFB JS -->
-<script src="js/sjfb-builder.js" type="text/javascript" ></script> <!-- form builder -->
-<script src="js/sjfb-html-generator.js" type="text/javascript" ></script> <!-- form generator -->
 <div id="sjfb-body">
 <div id="sjfb-wrap">
-
-<h1>Simple Jquery Form Builder - Demo</h1>
-
+<h1><?= $functionalitiesInstance->label('فرم‌ساز') ?></h1>
 <p>The form builder below was loaded in from a json string.<br>
-    <a href="form.html">View form (rendered from the demo json string) here</a>.
+<a href="form.html">View form (rendered from the demo json string) here</a>.
 </p>
-
-<div class="alert hide">
-    <h2>Success! Form saved.</h2>
-    <p>Here is what your saved form will look like in your database:</p>
-    <textarea></textarea>
-</div>
-
 <div class="add-wrap">
     <h3>Add Field:</h3>
     <ul id="add-field">
@@ -39,22 +22,20 @@ include ('forms/submit.php');
         <li><a id="add-agree" data-type="agree" href="#">Agree Box</a></li>
     </ul>
 </div>
-
 <form id="sjfb" novalidate>
     <div id="form-fields">
     </div>
     <button type="submit" class="submit">Save Form</button>
 </form>
+</div>
+</div>
 
-</div>
-</div>
 
 <?php
-/*
 $_GET['type'] = "QUST";
 require_once ('forms/render.php');
 require_once 'semi-orm/Posts.php';
 use orm\Posts;
 $post = new Posts($conn);
-// include ('master/public-footer.php');
+include ('master/public-footer.php');
 ?>
